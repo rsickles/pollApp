b998ecc2cbabb8dc968f611838a3806745c2bdf7
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-10-12 00:10:05 -0400

    Added canvas hw

--- public/scripts.js
+++ public/scripts.js
@@ -1,85 +1,102 @@
-// code from class example is used below and configured for assignment
+// Used http://www.html5canvastutorials.com/ and draw.js for help writing on canvas
 
-function doXMLHttpRequest() {
-  var xhr = new XMLHttpRequest();
+ var canvas = document.getElementById('myCanvas');
+ var context = canvas.getContext('2d');
 
-  xhr.onreadystatechange=function()  {
-   if (xhr.readyState==4) {
-     if(xhr.status == 200) {
-        processResponse(xhr.responseText);
-    } else {
-      responseArea.innerHTML="Error code " + xhr.status;
-    }
-   }
-  }
-  xhr.open("GET", "data.json", true);
-  xhr.send(null);
-  }
+ // taken from website draw.js
+ document.ontouchmove = function(e){ e.preventDefault();};
+ var canvastop = canvas.offsetTop;
+ var canvasleft = canvas.offsetLeft;
+ var shape = "";
+ var colors = ["green", "red", "blue", "yellow"];
+ var color = "";
 
-  function processResponse(responseJSON) {
-        var responseObject = JSON.parse(responseJSON);
-        var displayText =
-                "You have "
-                + responseObject.homework.length
-                + " assignments Received From Server!";
-        document.getElementById("responseArea").innerHTML = displayText;
-        $('#responseArea').css("color", "red").show("slow");
-        //now to fill table with data
-        var table = document.getElementById('assignments');
-        for (var i = 0; i<responseObject.homework.length; i++) {
-                var row = document.createElement("TR");
-                var homework = responseObject.homework[i];
-
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-
-                // Add some text to the new cells:
-                cell1.innerHTML = homework.className;
-                cell2.innerHTML = homework.assignment;
-
-                table.appendChild(row);
-                }
-        $('#assignments').show();
-        var info = "<h2></h2>";
-        $(info).appendTo(table).text("YOU HAVE A LOT OF WORK");
-        $( 'td' ).attr( "BGCOLOR", "blue" );
+ //this function draws the shape with the specified color and shape
+ function drawShape(shape,color,locX,locY) {
+      var centerX = locX;
+      var centerY = locY;
+      //first decide what shape to draw based on check boxes
+      if(shape=="rectangle")
+      {
+	      context.beginPath();
+	      context.rect(centerX, centerY, 200, 100);
+      }
+      if(shape=="circle")
+      {
+	      var radius = 70;
+	      context.beginPath();
+	      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      }
+      if(shape=="square")
+      {
+	      context.beginPath();
+	      context.rect(centerX, centerY, 100, 100);
+      }
+      //now choose random color and add to canvas
+      var color_number = Math.floor(Math.random() * 4);
+      if(color!=="random"){
+      	context.fillStyle = color;
+      }
+      else
+      {
+      	context.fillStyle = colors[color_number];
+    	}
+      context.fill();
+      context.lineWidth = 5;
+      context.strokeStyle = '#003300';
+      context.stroke();
 }
 
+ // taken from website draw.js
+ var doOnTouchStart = function(event){
+	  event.preventDefault();
+	  if(shape==="" || color ==="")
+	  {
+	  	alert("Please Select Shape And Color First");
+	  }
+	  else
+	  {
+	    lastx = event.touches[0].clientX - canvasleft;  // try substituting 1
+	    lasty = event.touches[0].clientY - canvastop;   // or 2 for index for multitouch
+	    drawShape(shape,color,lastx,lasty);
+  	}
+  };
 
-    // JQUERY SCRIPT
+ // used for dragging shape creation effect
+ var doOnTouchMove = function(event)
+ {
+    event.preventDefault();
 
-  $(function() {  // do once original document loaded and ready
-        $('#remove').click(function() {
-          $('html').find('#message').remove();
-          $('html').find('#responseArea').remove();
-        });
-        $('#grades').hide();
-        $('#assignments').hide();
-        $('#message' ).fadeIn( "slow");
-        $('#jquery').click(function() {
-                $.getJSON("DataTwo.json", function(responseObject, diditwork) {
-                        console.log(diditwork);
-                        var table = document.getElementById('grades');
-                        for (var i = 0; i<responseObject.grades.length; i++) {
-                                var object = responseObject.grades[i];
-                                var row = document.createElement("TR");
+    if(shape==="" || color ==="")
+	  {
+	  	alert("Please Select Shape And Color First");
+	  }
+	  else
+	  {
+    	var newx = event.touches[0].clientX - canvasleft;
+    	var newy = event.touches[0].clientY - canvastop;
+    	drawShape(shape,color,newx,newy);
+  	}
+  }
 
-                                var cell1 = row.insertCell(0);
-                                var cell2 = row.insertCell(1);
+ //called to get the shape of the currently checked box
+ function getShape(){
+ 	shape = $('input[name=shape]:checked', '#chooseShape').attr('id');
+ }
 
-                                // Add some text to the new cells:
-                                cell1.innerHTML = object.className;
-                                cell2.innerHTML = object.grade;
+//called to get the color of the currently checked box
+ function getColor(){
+ 	color = $('input[name=color]:checked', '#chooseColor').attr('id');
+ }
 
-                                table.appendChild(row);
+// added touch handlers
+canvas.addEventListener("touchmove", doOnTouchMove);
+canvas.addEventListener("touchstart", doOnTouchStart);
 
-                                }
-                                $('#grades').show();
+ // bind event handler to clear button
+document.getElementById('clear').addEventListener('click', function() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}, false);
 
-                                $('#message').css("color", "red").text("DO BETTER!");
-                                var info = "<h2></h2>";
-                                $(info).appendTo(table).text("Grades are ok.");
-                                $(table).append(info).show();
-                } );  // getJSON
-        } );  // click
-  } ); // onReady
+
+
