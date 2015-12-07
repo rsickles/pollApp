b6412098d71cd1088f53492d1374c4218ada893d
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-12-07 02:47:13 -0500

    asdf

--- pollapp/server.js
+++ pollapp/server.js
@@ -35,7 +35,13 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
 // error handlers
 
 // development error handler
